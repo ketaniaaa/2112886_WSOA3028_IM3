2df08f1fc43e8d4d0dbab81d94b71a8e3450e5df
Ketania Govender <2112886@students.wits.ac.za>
2021-06-18 10:34:56 +0200

    new featured section grd

---
+++ scripts/slideshow.js
@@ -0,0 +1,8 @@
+var counter = 1;
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 4){
+    counter = 1;
+  }
+}, 5000);  //a simple slideshow for the banner of the website
